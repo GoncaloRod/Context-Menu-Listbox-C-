c6b83c24327b98f9cadcc3f2ff1bcebbc26be99f
GoncaloRod <rodriguesgonalo604@gmail.com>
2017-02-20 21:48:26 +0000

    Simple way to show context menu on Listbox item in C#

---
+++ ListBoxContextMenu/Main.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ListBoxContextMenu
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstPessoas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstPessoas
+            // 
+            this.lstPessoas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstPessoas.FormattingEnabled = true;
+            this.lstPessoas.Location = new System.Drawing.Point(12, 12);
+            this.lstPessoas.Name = "lstPessoas";
+            this.lstPessoas.Size = new System.Drawing.Size(260, 173);
+            this.lstPessoas.TabIndex = 0;
+            this.lstPessoas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lstPessoas_MouseDown);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 199);
+            this.Controls.Add(this.lstPessoas);
+            this.Name = "Main";
+            this.Text = "Context Menu List Box";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstPessoas;
+    }
+}
+
---
+++ ListBoxContextMenu/Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxContextMenu
+{
+    public partial class Main : Form
+    {
+        // Lista de pessoas
+        List<string> pessoas = new List<string>();
+
+        // Menu de contexto
+        ContextMenu cm;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            // Configurar o menu de contexto
+            cm = new ContextMenu();
+            cm.MenuItems.Add("Editar", Editar);
+            cm.MenuItems.Add("Eliminar", Eliminar);
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            pessoas.Add("José");
+            pessoas.Add("Manel");
+            pessoas.Add("Josefino");
+            pessoas.Add("Mongoloide");
+
+            lstPessoas.DataSource = pessoas;
+        }
+
+        private void lstPessoas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                var item = lstPessoas.IndexFromPoint(e.Location);
+                if (item >= 0)
+                {
+                    lstPessoas.SelectedIndex = item;
+                    cm.Show(lstPessoas, e.Location);
+                }
+            }
+        }
+
+        private void Editar(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Pessoa Editada! Item: {lstPessoas.SelectedIndex}");
+        }
+
+        private void Eliminar(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Pessoa Eliminada! Item: {lstPessoas.SelectedIndex}");
+        }
+    }
+}
